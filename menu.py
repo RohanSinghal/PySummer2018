d520989ff44a3952013d90d62d772a1cd1995084
VatsalBabel <vatsalbabel06@gmail.com>
2018-05-17 01:48:14 +0530

    Complete menu functionality

--- menu.py
+++ menu.py
@@ -6,11 +6,13 @@ import webbrowser
 import requests
 from bs4 import BeautifulSoup
 
-print("Choose an option-\n1. Google Search\n2. Google Image Search\n3. Finding the URL\n4. Current time and date\n5. Open Web Browser\n6. IP of all the network\n7. Verify EmailId and contact")
+print("Choose an option-\n1. Google Search\n2. Google Image Search\n3. Finding the URL\n4. Current time and date\n5. Open Web Browser\n6. IP of all the network\n7. Find EmailId and contact")
 
+#Taking option input from the user
 option = int(input())
 
 if option==1:
+	#Making a google search
 	search = input("Enter a string to search on google - ")
 	list1 = search.split()
 	for i in list1:
@@ -18,6 +20,7 @@ if option==1:
 		webbrowser.open_new_tab(search)
 
 elif option==2:
+	#Making a google image search
 	search = input("Enter a string to search on google images - ")
 	list1 = search.split()
 	for i in list1:
@@ -25,7 +28,8 @@ elif option==2:
 		webbrowser.open_new_tab(search)
 
 elif option==3:
-	search = input("Enter a string to search on google - ")
+	#Scrapting to find the URL from the first page of google
+	search = input("Enter a string to find the related url from google - ")
 	list1 = search.split()
 	for i in list1:
 		search = "https://www.google.co.uk/search?q=" + i
@@ -41,13 +45,36 @@ elif option==3:
 				print(url[:index])
 
 elif option==4:
+	#Getting the current date and the time
 	print(time.asctime())
 
 elif option==5:
+	#Opening the web browser
 	webbrowser.open_new_tab('https://')
 
 elif option==6:
+	#Finding the IP of connected system
 	os.system('nmap -sP 10.0.0.0/24')
 
 else:
-	pass
+	#Finding the EmailId and contact number of the admin of the given error
+	search = input("Enter the domain name of the website - ")
+	search = "whois "+ search
+	#Taking console output in a string and filtering the string
+	result = os.popen(search).read()
+	index = result.find('Admin Phone')
+	result = result[index::]
+	index = result.find('.com')
+	result = result[:index+4]
+	list1 = result.splitlines()
+	index = 0
+	#Finding the email and contant of the admin from the string
+	for i in range(len(list1)):
+		list1[index] = str(list1[index]).strip()
+		if list1[index].count('Ext')>0 or list1[index].count('Fax')>0:
+			list1.pop(index)
+		else:
+			print(list1[index])
+			index += 1
+
+	
